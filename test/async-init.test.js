334cdf085f80e54ec58909d2a0efe18acf9ca7bc
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-04-23 00:33:09 -0400

    Add additional tests for ready state after async init

--- test/async-init.test.js
+++ test/async-init.test.js
@@ -8,7 +8,7 @@ const { EVT_READY, EVT_NO_INIT_WARN } = require("../src");
  */
 
 test("instantiates async", async t => {
-  t.plan(3);
+  t.plan(5);
 
   const phantom = new PhantomBase({
     isReady: false,
@@ -22,10 +22,25 @@ test("instantiates async", async t => {
     })
   );
 
+  phantom.once(EVT_READY, () => {
+    t.ok(true, "emits EVT_READY after async init");
+  });
+
   t.notOk(phantom.getIsReady(), "not ready before init");
 
   // This is a protected method which shouldn't normally be called by the implementer
-  await phantom._init();
+  await Promise.all([
+    // This is a protected method which shouldn't normally be called by the implementer
+    phantom._init(),
+
+    // Called right after _init
+    phantom.onceReady(),
+  ]);
+
+  t.ok(
+    undefined === (await phantom.onceReady()),
+    "subsequent calls to onceReady pass through"
+  );
 
   t.ok(phantom.getIsReady(), "ready after init");
 });
