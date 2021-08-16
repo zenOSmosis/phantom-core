7b75d3536d53a88d59f32c08f78ede3ce879531b
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-08-15 18:15:12 -0600

    Fix event plumbing

--- src/PhantomCollection/index.js
+++ src/PhantomCollection/index.js
@@ -2,3 +2,5 @@ const PhantomCollection = require("./PhantomCollection");
 const { EVT_UPDATED, EVT_DESTROYED } = PhantomCollection;
 
 module.exports = PhantomCollection;
+module.exports.EVT_UPDATED = EVT_UPDATED;
+module.exports.EVT_DESTROYTED = EVT_DESTROYED;
