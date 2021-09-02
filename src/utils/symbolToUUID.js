1193deafaefd8d3060ec17cffedbddf5fec32d45
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-09-01 20:33:45 -0600

    Add description

--- src/utils/symbolToUUID.js
+++ src/utils/symbolToUUID.js
@@ -1,6 +1,15 @@
 const uuidv4 = require("uuid").v4;
 
-// TODO: Document
+/**
+ * Retrieves the same UUID for same symbols.
+ *
+ * Useful for using symbol in places where a string or number should be used,
+ * instead.
+ *
+ * @param {Symbol} symbol
+ * @return {string} A 36-character string;
+ * i.e. "d64e14a5-123b-436a-b168-18c517e3793c"
+ */
 const symbolToUUID = (() => {
   const symbolMap = new Map();
 
