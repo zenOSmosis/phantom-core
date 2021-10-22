3683c84b7343dc07ec03887d0c6d68df2ce5fd08
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-22 11:38:02 -0600

    Add TODOs

--- node-utils/writeVersionInfo.js
+++ node-utils/writeVersionInfo.js
@@ -4,6 +4,9 @@ const packageInfo = require("./packageInfo");
 
 const { version } = packageInfo;
 
+// TODO: Maybe this isn't needed afterall, just import directly from package.json
+// @see https://github.com/microsoft/OCR-Form-Tools/blob/master/src/common/appInfo.ts
+
 // Writes the version info to a static file which is included in PhantomCore
 writeFileSync(
   path.resolve(__dirname, "..", "src", "static", "version.js"),
--- src/static/version.js
+++ src/static/version.js
@@ -1,1 +1,3 @@
+// TODO: Can we just import package.info directly?
+
 module.exports = "2.0.0-alpha";
