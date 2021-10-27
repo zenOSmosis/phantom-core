76f132488cddf7f8af77f522098d8d9f31e4a766
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-27 18:37:16 -0500

    Add re-bind tests

--- test/auto-bind.test.js
+++ test/auto-bind.test.js
@@ -61,3 +61,62 @@ test("PhantomCore method auto-binding", async t => {
 
   t.end();
 });
+
+test("auto-bind no re-bind", t => {
+  t.plan(2);
+
+  class TestPhantom1 extends PhantomCore {}
+  class TestPhantom2 extends PhantomCore {}
+
+  const p1 = new TestPhantom1();
+  const p2 = new TestPhantom2();
+
+  // Attempt cross instance rebinding
+  // NOTE: This should not re-bind due to bind preventing re-binds
+  p1.getClassName = p1.getClassName.bind(p2);
+  p2.getClassName = p2.getClassName.bind(p1);
+
+  t.equals(
+    p1.getClassName(),
+    "TestPhantom1",
+    "subsequent cross-binding has no effect on auto-bound instance 1"
+  );
+  t.equals(
+    p2.getClassName(),
+    "TestPhantom2",
+    "subsequent cross-binding has no effect on auto-bound instance 2"
+  );
+
+  t.end();
+});
+
+test("non-auto-bind subsequent bind", t => {
+  t.plan(2);
+
+  class TestPhantom3 extends PhantomCore {}
+  class TestPhantom4 extends PhantomCore {}
+
+  const p3 = new TestPhantom3({
+    hasAutomaticBindings: false,
+  });
+
+  const p4 = new TestPhantom4({
+    hasAutomaticBindings: false,
+  });
+
+  // Attempt cross instance binding
+  // NOTE: These should bind due to hasAutomaticBindings being disabled
+  p3.getClassName = p3.getClassName.bind(p4);
+  p4.getClassName = p4.getClassName.bind(p3);
+
+  t.notEquals(
+    p3.getClassName(),
+    "TestPhantom3",
+    "subsequent cross-binding has effect on non-auto-bound instance 3"
+  );
+  t.notEquals(
+    p4.getClassName(),
+    "TestPhantom4",
+    "subsequent cross-binding has effect on non-auto-bound instance 4"
+  );
+});
