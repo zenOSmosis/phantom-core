769ce1984bb175f2ad98751a3d46a60d75d4af31
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-11-06 10:34:37 -0500

    Add additional exports and apply additional housekeeping

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 const PhantomCore = require("./PhantomCore");
 const { EVT_READY, EVT_UPDATED, EVT_DESTROYED, EVT_NO_INIT_WARN } = PhantomCore;
 
-module.exports = PhantomCore
+module.exports = PhantomCore;
 module.exports.EVT_READY = EVT_READY;
 module.exports.EVT_UPDATED = EVT_UPDATED;
 module.exports.EVT_DESTROYED = EVT_DESTROYED;
 module.exports.EVT_NO_INIT_WARN = EVT_NO_INIT_WARN;
-
-module.exports.PhantomCollection = require("./PhantomCollection");
-module.exports.PhantomServiceManager = require("./PhantomServiceManager");
-module.exports.PhantomServiceCore = require("./PhantomServiceCore");
-module.exports.PhantomState = require("./PhantomState");
-module.exports.PhantomSerializableState = require("./PhantomSerializableState");
 
 module.exports.Logger = require("./Logger");
 const {
@@ -30,18 +24,35 @@ module.exports.LOG_LEVEL_WARN = LOG_LEVEL_WARN;
 module.exports.LOG_LEVEL_ERROR = LOG_LEVEL_ERROR;
 module.exports.LOG_LEVEL_SILENT = LOG_LEVEL_SILENT;
 
-// Independent logger (outside of PhantomCore instance; used instead of
-// console.log)
-module.exports.logger = new module.exports.Logger()
+// Global logger instance
+module.exports.logger = new module.exports.Logger();
 
+module.exports.PhantomCollection = require("./PhantomCollection");
+module.exports.PhantomServiceCore = require("./PhantomServiceCore");
+module.exports.PhantomServiceManager = require("./PhantomServiceManager");
+module.exports.PhantomSerializableState = require("./PhantomSerializableState");
+module.exports.PhantomState = require("./PhantomState");
+
+module.exports.FunctionStack = require("./FunctionStack");
+
+// Base utilities
+module.exports.deepMerge = require("./utils/deepMerge");
+module.exports.getIsNodeJS = require("./utils/getIsNodeJS");
+module.exports.getPackageJSON = require("./utils/getPackageJSON");
 module.exports.getUnixTime = require("./utils/getUnixTime");
 module.exports.getUptime = require("./utils/getUptime");
-module.exports.getIsNodeJS = require("./utils/getIsNodeJS");
-module.exports.deepMerge = require("./utils/deepMerge");
-module.exports.getClassName = require("./utils/class-utils/getClassName");
+module.exports.shallowMerge = require("./utils/shallowMerge");
+
+// Utilities for working with JavaScript classes
+module.exports.autoBindClassInstanceMethods = require("./utils/class-utils/autoBindClassInstanceMethods");
+module.exports.getClass = require("./utils/class-utils/getClass");
 module.exports.getClassInheritance = require("./utils/class-utils/getClassInheritance");
+module.exports.getClassInstanceMethodNames = require("./utils/class-utils/getClassInstanceMethodNames");
+module.exports.getClassInstancePropertyNames = require("./utils/class-utils/getClassInstancePropertyNames");
+module.exports.getClassName = require("./utils/class-utils/getClassName");
+module.exports.getIsClass = require("./utils/class-utils/getIsClass");
+module.exports.getIsClassInstance = require("./utils/class-utils/getIsClassInstance");
 module.exports.getSuperClass = require("./utils/class-utils/getSuperClass");
-// const symbolToUUID = require("./utils/symbolToUUID");
 
+// Utility for checking PhantomCore (and extension) event exports
 module.exports.eventConstantCheckingUtils = require("./utils/testing-utils/eventConstantCheckingUtils");
-
