684e8f35ca46549d40b8be6c8e0aa7dfa2d7a52d
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-11-06 10:08:36 -0500

    Rework main exports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const PhantomCore = require("./PhantomCore");
 const { EVT_READY, EVT_UPDATED, EVT_DESTROYED, EVT_NO_INIT_WARN } = PhantomCore;
-const PhantomCollection = require("./PhantomCollection");
-const PhantomServiceManager = require("./PhantomServiceManager");
-const PhantomServiceCore = require("./PhantomServiceCore");
-const PhantomState = require("./PhantomState");
-const PhantomSerializableState = require("./PhantomSerializableState");
-const Logger = require("./Logger");
+
+module.exports = PhantomCore
+module.exports.EVT_READY = EVT_READY;
+module.exports.EVT_UPDATED = EVT_UPDATED;
+module.exports.EVT_DESTROYED = EVT_DESTROYED;
+module.exports.EVT_NO_INIT_WARN = EVT_NO_INIT_WARN;
+
+module.exports.PhantomCollection = require("./PhantomCollection");
+module.exports.PhantomServiceManager = require("./PhantomServiceManager");
+module.exports.PhantomServiceCore = require("./PhantomServiceCore");
+module.exports.PhantomState = require("./PhantomState");
+module.exports.PhantomSerializableState = require("./PhantomSerializableState");
+
+module.exports.Logger = require("./Logger");
 const {
   LOG_LEVEL_TRACE,
   LOG_LEVEL_DEBUG,
@@ -13,25 +21,7 @@ const {
   LOG_LEVEL_WARN,
   LOG_LEVEL_ERROR,
   LOG_LEVEL_SILENT,
-} = Logger;
-
-const getUnixTime = require("./utils/getUnixTime");
-const getUptime = require("./utils/getUptime");
-const getIsNodeJS = require("./utils/getIsNodeJS");
-const deepMerge = require("./utils/deepMerge");
-const getClassName = require("./utils/class-utils/getClassName");
-const getClassInheritance = require("./utils/class-utils/getClassInheritance");
-const getSuperClass = require("./utils/class-utils/getSuperClass");
-// const symbolToUUID = require("./utils/symbolToUUID");
-
-const eventConstantCheckingUtils = require("./utils/testing-utils/eventConstantCheckingUtils");
-
-module.exports = PhantomCore;
-module.exports.PhantomCollection = PhantomCollection;
-module.exports.EVT_READY = EVT_READY;
-module.exports.EVT_UPDATED = EVT_UPDATED;
-module.exports.EVT_DESTROYED = EVT_DESTROYED;
-module.exports.EVT_NO_INIT_WARN = EVT_NO_INIT_WARN;
+} = module.exports.Logger;
 
 module.exports.LOG_LEVEL_TRACE = LOG_LEVEL_TRACE;
 module.exports.LOG_LEVEL_DEBUG = LOG_LEVEL_DEBUG;
@@ -42,20 +32,16 @@ module.exports.LOG_LEVEL_SILENT = LOG_LEVEL_SILENT;
 
 // Independent logger (outside of PhantomCore instance; used instead of
 // console.log)
-module.exports.logger = new Logger();
+module.exports.logger = new module.exports.Logger()
 
-module.exports.PhantomServiceManager = PhantomServiceManager;
-module.exports.PhantomServiceCore = PhantomServiceCore;
-module.exports.PhantomState = PhantomState;
-module.exports.PhantomSerializableState = PhantomSerializableState;
+module.exports.getUnixTime = require("./utils/getUnixTime");
+module.exports.getUptime = require("./utils/getUptime");
+module.exports.getIsNodeJS = require("./utils/getIsNodeJS");
+module.exports.deepMerge = require("./utils/deepMerge");
+module.exports.getClassName = require("./utils/class-utils/getClassName");
+module.exports.getClassInheritance = require("./utils/class-utils/getClassInheritance");
+module.exports.getSuperClass = require("./utils/class-utils/getSuperClass");
+// const symbolToUUID = require("./utils/symbolToUUID");
 
-module.exports.getUnixTime = getUnixTime;
-module.exports.getUptime = getUptime;
-module.exports.getIsNodeJS = getIsNodeJS;
-module.exports.deepMerge = deepMerge;
-module.exports.getClassName = getClassName;
-module.exports.getClassInheritance = getClassInheritance;
-module.exports.getSuperClass = getSuperClass;
-// module.exports.symbolToUUID = symbolToUUID;
+module.exports.eventConstantCheckingUtils = require("./utils/testing-utils/eventConstantCheckingUtils");
 
-module.exports.eventConstantCheckingUtils = eventConstantCheckingUtils;
