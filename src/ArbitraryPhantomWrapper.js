5b62e8b155f006b50fced397d17f5ee1c54d5c6d
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-12-16 17:33:31 -0600

    Implement ArbitraryPhantomWrapper and bump to v2.2.0

---
+++ src/ArbitraryPhantomWrapper.js
@@ -0,0 +1,63 @@
+const PhantomCore = require("./PhantomCore");
+const { EVT_NO_INIT_WARN, EVT_READY, EVT_UPDATED, EVT_DESTROYED } = PhantomCore;
+
+/**
+ * Wraps an arbitrary object with
+ */
+class ArbitraryPhantomWrapper extends PhantomCore {
+  /**
+   * @param {any} wrappedValue
+   * @param {Object} options? [default = {}]
+   */
+  constructor(wrappedValue, options = {}) {
+    super(options);
+
+    this._wrappedValue = null;
+    this._setWrappedValue(wrappedValue);
+  }
+
+  /**
+   * @return {Promise<void>}
+   */
+  async destroy() {
+    await super.destroy();
+
+    // IMPORTANT: Setting this AFTER super destroy so that it can potentially
+    // be intercepted by other destruct handlers in extension classes
+    this._wrappedValue = null;
+  }
+
+  /**
+   * Internally sets the wrapped element.
+   *
+   * @param {any} wrappedValue
+   * @return {void}
+   */
+  _setWrappedValue(wrappedValue) {
+    if (this._wrappedValue) {
+      throw new Error("_setWrappedValue cannot be called more than once");
+    }
+
+    if (!wrappedValue) {
+      throw new ReferenceError("wrappedValue is not set");
+    }
+
+    this._wrappedValue = wrappedValue;
+  }
+
+  /**
+   * Retrieves the wrapped object which was specified during the class instance
+   * creation.
+   *
+   * @return {any}
+   */
+  getWrappedValue() {
+    return this._wrappedValue;
+  }
+}
+
+module.exports = ArbitraryPhantomWrapper;
+module.exports.EVT_NO_INIT_WARN = EVT_NO_INIT_WARN;
+module.exports.EVT_READY = EVT_READY;
+module.exports.EVT_UPDATED = EVT_UPDATED;
+module.exports.EVT_DESTROYED = EVT_DESTROYED;
