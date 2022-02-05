de96a38bab7f312bd560d60265945d6eb0795728
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-02-05 08:07:08 -0600

    Remove debug destroyPhase and checking for destroyHandlerStack

--- src/_DestructibleEventEmitter.js
+++ src/_DestructibleEventEmitter.js
@@ -31,9 +31,6 @@ module.exports = class DestructibleEventEmitter extends EventEmitter {
 
     this._destroyHandlerStack = new FunctionStack();
 
-    // TODO: Remove?
-    this._destroyPhase = null;
-
     // Prevent incorrect usage of EVT_DESTROYED; EVT_DESTROYED should only be
     // emit internally during the shutdown phase
     this.on(EVT_DESTROYED, () => {
@@ -103,9 +100,6 @@ module.exports = class DestructibleEventEmitter extends EventEmitter {
     if (!this._isDestroying) {
       this._isDestroying = true;
 
-      // TODO: Remove?
-      this._destroyPhase = "a";
-
       this.emit(EVT_BEFORE_DESTROY);
 
       this._destroyHandlerStack.push(destroyHandler);
@@ -117,19 +111,10 @@ module.exports = class DestructibleEventEmitter extends EventEmitter {
       // This try / catch fixes an issue where an error in the callstack
       // doesn't clear the longRespondDestroyHandlerTimeout
       try {
-        // TODO: Remove?
-        this._destroyPhase = "b";
-
         await this._destroyHandlerStack.exec();
       } catch (err) {
-        // TODO: Remove?
-        this._destroyPhase = "be";
-
         throw err;
       } finally {
-        // TODO: Remove?
-        this._destroyPhase = "c";
-
         clearTimeout(longRespondDestroyHandlerTimeout);
 
         // Remove remaining functions from stack, if exist (this should
@@ -138,13 +123,7 @@ module.exports = class DestructibleEventEmitter extends EventEmitter {
 
         // Remove reference to destroy handler stack
         this._destroyHandlerStack = null;
-
-        // TODO: Remove?
-        this._destroyPhase = "d";
       }
-
-      // TODO: Remove?
-      this._destroyPhase = "e";
 
       // Set the state before the event is emit so that any listeners will know
       // the correct state
@@ -156,12 +135,9 @@ module.exports = class DestructibleEventEmitter extends EventEmitter {
       // Remove all event listeners; we're stopped
       this.removeAllListeners();
 
-      // TODO: Remove?
-      this._destroyPhase = "f";
-
       // No longer in "destroying" phase, and destroyed at this point
       // this._isDestroying = false;
-    } else if (this._destroyHandlerStack) {
+    } else {
       // Enable subsequent call with another destroyHandler; this fixes an
       // issue with Chrome / Safari MediaStreamTrackControllerFactory not
       // properly emitting EVT_UPDATED when a child controller is destructed
@@ -175,20 +151,6 @@ module.exports = class DestructibleEventEmitter extends EventEmitter {
       // Wait for the instance to be destroyed before resolving (all subsequent
       // destroy() calls should resolve at the same time)
       return new Promise(resolve => this.once(EVT_DESTROYED, resolve));
-    } else {
-      // FIXME: (jh) This is able to be reproduced when rapidly starting and
-      // stopping an input media device in ReShell, and I'm not yet sure why it's
-      // happening.  Rather than throw, I want to treat it more as a warning
-      // for the time being.
-      console.error(
-        "Could not add new destroyHandler to an already destructed destroyHandlerStack",
-        {
-          isDestroyed: this._isDestroyed,
-          isDestroying: this._isDestroying,
-          destroyPhase: this._destroyPhase,
-          destroyHandler: destroyHandler.toString(),
-        }
-      );
     }
   }
 };
