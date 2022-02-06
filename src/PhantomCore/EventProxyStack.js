bf354b63621dfb428c4a1801f5f731dab4c126ae
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-02-06 10:32:18 -0600

    Document EventProxyStack

--- src/PhantomCore/EventProxyStack.js
+++ src/PhantomCore/EventProxyStack.js
@@ -1,36 +1,62 @@
+const PhantomCore = require(".");
 const _DestructibleEventEmitter = require("../_DestructibleEventEmitter");
 const { EVT_DESTROYED } = _DestructibleEventEmitter;
 
-// TODO: Document
 module.exports = class EventProxyStack extends _DestructibleEventEmitter {
+  /**
+   * Provides event proxy management for PhantomCore's, proxyOn, proxyOnce, and
+   * proxyOff methods.
+   *
+   * Internally it binds the events to the remote target instance(s) and manages
+   * their handling.
+   */
   constructor() {
     super();
 
-    // TODO: Document structure
+    /**
+     * @type {[targetInstance: PhantomCore, eventName: string, eventHandler: Function]}
+     */
     this._eventProxyBinds = [];
 
-    // TODO: Document structure
+    /**
+     * @type {Map<{key: PhantomCore, value: Function}>}
+     */
     this._targetDestroyHandlers = new Map();
   }
 
-  // TODO: Document
+  /**
+   *
+   * @param {"on" | "once"} onOrOnce
+   * @param {PhantomCore} targetInstance
+   * @param {string} eventName
+   * @param {Function} eventHandler
+   * @return {void}
+   */
   addProxyHandler(onOrOnce, targetInstance, eventName, eventHandler) {
+    if (onOrOnce !== "on" && onOrOnce !== "once") {
+      throw new ReferenceError(`Unhandled value "${onOrOnce}" for onOrOnce`);
+    }
+
     this._eventProxyBinds.push([targetInstance, eventName, eventHandler]);
 
     // Bind the event handler to the target instance
     targetInstance[onOrOnce](eventName, eventHandler);
 
-    // Handle scenario where targetInstance is destructed before local instance
+    // If there is not a target destroy handler...
     if (!this._targetDestroyHandlers.get(targetInstance)) {
+      // ... create one
       this._targetDestroyHandlers.set(targetInstance, () => {
-        // Invoke proxyOff for bound eventHandlers
         this._eventProxyBinds
+          // IMPORTANT: Only invoke proxyOff for bound eventHandlers for the
+          // correct target instance
           .filter(
             ([predTargetInstance]) => predTargetInstance === targetInstance
           )
           .forEach(args => this.removeProxyHandler(...args));
       });
 
+      // ... then register the target destroy handler to run once the target
+      // emits EVT_DESTROYED
       targetInstance.once(
         EVT_DESTROYED,
         this._targetDestroyHandlers.get(targetInstance)
@@ -38,12 +64,26 @@ module.exports = class EventProxyStack extends _DestructibleEventEmitter {
     }
   }
 
-  // TODO: Document
+  /**
+   * Removes the given proxy handler
+   *
+   * @param {PhantomCore} targetInstance
+   * @param {string} eventName
+   * @param {Function} eventHandler
+   * @return {void}
+   */
   removeProxyHandler(targetInstance, eventName, eventHandler) {
     let hasRemoved = false;
 
     this._eventProxyBinds = this._eventProxyBinds.filter(
       ([predTargetInstance, predEventName, predEventHandler]) => {
+        // If an associated event handler is added more than once, for the
+        // given event name, only remove it once from the _eventProxyBinds.
+        // This target instance's event emitter manages how it wishes to keep
+        // track of duplicated event handlers across on / once. Our job here is
+        // to not try to manage that here, but just keep track of the
+        // associated proxies, one-by-one, so we can ensure we fully unregister
+        // them when we shut down.
         if (hasRemoved) {
           return true;
         } else {
@@ -72,7 +112,37 @@ module.exports = class EventProxyStack extends _DestructibleEventEmitter {
     targetInstance.off(eventName, eventHandler);
   }
 
-  // TODO: Document
+  /**
+   * Removes all proxy handlers for all of the target instances.
+   *
+   * @return {void}
+   */
+  _removeAllProxyHandlers() {
+    this._eventProxyBinds.forEach(([targetInstance, eventName, eventHandler]) =>
+      this.removeProxyHandler(targetInstance, eventName, eventHandler)
+    );
+  }
+
+  /**
+   * Unregisters all remote destroy handlers for the target instances.
+   *
+   * @return {void}
+   */
+  _removeAllTargetDestroyHandlers() {
+    for (const [targetInstance, destroyHandler] of this
+      ._targetDestroyHandlers) {
+      targetInstance.off(EVT_DESTROYED, destroyHandler);
+
+      this._targetDestroyHandlers.delete(targetInstance);
+    }
+  }
+
+  /**
+   * Retrieves the number of proxied events to the given target instance.
+   *
+   * @param {PhantomCore} targetInstance
+   * @return {number}
+   */
   getTargetInstanceQueueDepth(targetInstance) {
     return this._eventProxyBinds.reduce((acc, curr) => {
       const predTargetInstance = curr[0];
@@ -89,22 +159,16 @@ module.exports = class EventProxyStack extends _DestructibleEventEmitter {
    */
   async destroy() {
     return super.destroy(() => {
-      this._eventProxyBinds.forEach(
-        ([targetInstance, eventName, eventHandler]) =>
-          this.removeProxyHandler(targetInstance, eventName, eventHandler)
-      );
+      this._removeAllProxyHandlers();
 
+      // Shutdown checking
       if (this._eventProxyBinds.length > 0) {
         throw new Error("Did not successfully unregister event proxy binds");
       }
 
-      for (const [targetInstance, destroyHandler] of this
-        ._targetDestroyHandlers) {
-        targetInstance.off(EVT_DESTROYED, destroyHandler);
+      this._removeAllTargetDestroyHandlers();
 
-        this._targetDestroyHandlers.delete(targetInstance);
-      }
-
+      // Shutdown checking
       if ([...this._targetDestroyHandlers].length > 0) {
         throw new Error(
           "Did not successfully unregister target destroy handlers"
