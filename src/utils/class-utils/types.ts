defbfa420690a0bb54355b4deccea9ea9b8780e6
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-05-22 15:46:43 -0500

    Add support for generic Class type

--- src/utils/class-utils/types.ts
+++ src/utils/class-utils/types.ts
@@ -1,5 +1,10 @@
+interface IClass {
+  new (): IClass;
+}
+
+// TODO: [3.0.0] Fix any type
 // Borrowed from: https://stackoverflow.com/questions/39392853/is-there-a-type-for-class-in-typescript-and-does-any-include-it
-export type Class = { new (...args: any[]): any };
+export type Class<T = any> = IClass;
 
 // TODO: Remove this... or rename and use intersection type
 // TODO: [3.0.0] Fix any type
