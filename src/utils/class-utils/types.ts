bdfa69709960fc799800c862962fabc696f1568b
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-05-23 06:04:05 +0000

    Update types

--- src/utils/class-utils/types.ts
+++ src/utils/class-utils/types.ts
@@ -1,10 +1,10 @@
-interface IClass {
-  new (): IClass;
-}
+type Instantiable = {
+  new (...args: any[]): Instantiable;
+};
 
 // TODO: [3.0.0] Fix any type
 // Borrowed from: https://stackoverflow.com/questions/39392853/is-there-a-type-for-class-in-typescript-and-does-any-include-it
-export type Class<T = any> = IClass;
+export type Class<T = Instantiable> = Instantiable | T;
 
 // TODO: Remove this... or rename and use intersection type
 // TODO: [3.0.0] Fix any type
