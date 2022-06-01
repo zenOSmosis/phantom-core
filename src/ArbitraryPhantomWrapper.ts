3c9a6149c79c08d1ca46bcfb0a9b5e16add5412f
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-06-01 12:46:09 +0000

    Make _setWrappedValue a protected method

--- src/ArbitraryPhantomWrapper.ts
+++ src/ArbitraryPhantomWrapper.ts
@@ -29,7 +29,7 @@ export default class ArbitraryPhantomWrapper extends PhantomCore {
   }
 
   // TODO: [3.0.0] Document
-  _setWrappedValue(wrappedValue: unknown): void {
+  protected _setWrappedValue(wrappedValue: unknown): void {
     if (this._wrappedValue) {
       throw new Error("_setWrappedValue cannot be called more than once");
     }
