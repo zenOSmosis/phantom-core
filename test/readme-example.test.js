77c1fd3f7b350ea90037e25558dc99959b15dbff
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-06-14 02:36:38 +0000

    Update README example

--- test/readme-example.test.js
+++ test/readme-example.test.js
@@ -6,21 +6,28 @@ test("basic example", async t => {
     constructor() {
       super();
 
-      // Cleanup handlers are invoked in LIFO order by default
+      // Cleanup handlers are invoked in LIFO order by default, so that cleanup handlers can be placed consecutively after certain operations
 
-      this.registerCleanupHandler(() => {
-        this.log("And some additional cleanup work here...");
+      this._a = new PhantomCore();
+      this.registerCleanupHandler(async () => {
+        this.log("Finish the stack cleanup...");
+
+        // Referenced PhantomCore instances must be destructed or disassociated, otherwise a warning will be raised about a potential memory leak
+        await this._a.destroy();
       });
 
+      this._b = { echo: 123 };
       this.registerCleanupHandler(() => {
-        // PhantomCore has its own logger, and the log level is configurable per
-        // instance. It's currently a wrapper around the Console log methods, has
-        // a custom prefix, and retains the original stack trace.
-        this.log("Do some cleanup work here...");
+        // Each PhantomCore instance has its own logger
+        this.log(
+          "Last defined gets cleaned up first, to more easily unwrap the stack on cleanup"
+        );
+
+        this._b = null;
       });
 
-      // Asynchronous methods in registerCleanupHandler are executed awaited upon
-      // as if they were run synchronously, to eliminate race conditions
+      // Asynchronous methods in registerCleanupHandler are awaited upon before
+      // the next item's invocation to eliminate race conditions
     }
   }
 
@@ -36,11 +43,10 @@ test("basic example", async t => {
     });
 
     ext.once(EVT_DESTROY, () => {
-      // Outside of an instance, the global logger can be used
       ext.log("Extension destroyed");
     });
 
-    // Destruct logs are rendered in the following order:
+    // Log output:
     //
     // [2022-06-14T01:46:52+00:00 info MyExtension 655eba48-892a-4d4d-8f45-a4ab20204679] Ready...
     // [2022-06-14T01:46:52+00:00 info MyExtension 655eba48-892a-4d4d-8f45-a4ab20204679] Extension destroyed
