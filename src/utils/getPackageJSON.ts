0d706b27a0a2571e75ba9f9720a301446419d205
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-06-14 00:09:26 +0000

    Fix types and imports

--- src/utils/getPackageJSON.ts
+++ src/utils/getPackageJSON.ts
@@ -1,15 +1,9 @@
-const packageJson = require("../../package.json");
+import packageJson from "../../package.json";
+import { RecursiveObject } from "../types";
 
 /**
  * Retrieves the package JSON data for PhantomCore.
  */
-export default function getPackageJSON(): {
-  // TODO: [3.0.0] Make recursive type
-  [key: string]:
-    | string
-    | number
-    | boolean
-    | { [key: string]: string | number | boolean };
-} {
+export default function getPackageJSON(): RecursiveObject {
   return packageJson;
 }
