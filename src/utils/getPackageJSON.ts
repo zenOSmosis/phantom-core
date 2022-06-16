2f23a30e9f7b7e902878dc64a003bfc52e02edea
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-06-16 21:37:00 +0000

    Fix issue where src was being added to dist directory

--- src/utils/getPackageJSON.ts
+++ src/utils/getPackageJSON.ts
@@ -1,9 +1,13 @@
-import packageJson from "../../package.json";
 import { RecursiveObject } from "../types";
+
+// Package.json is required instead of imported due to resolveJsonModule
+// prepending /src in dist directory
+// @see https://stackoverflow.com/a/58941798
+const packageJSON = require("../../package.json");
 
 /**
  * Retrieves the package JSON data for PhantomCore.
  */
 export default function getPackageJSON(): RecursiveObject {
-  return packageJson;
+  return packageJSON;
 }
