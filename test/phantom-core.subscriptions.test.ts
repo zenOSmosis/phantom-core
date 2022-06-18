b68ba26155fb7f2899e8fa824387adf54135c050
Jeremy Harris <jeremy.harris@zenosmosis.com>
2022-06-18 15:06:44 -0500

    Add multiple event testing

--- test/phantom-core.subscriptions.test.ts
+++ test/phantom-core.subscriptions.test.ts
@@ -24,6 +24,38 @@ test("subscribe / unsubscribe -- default event names", async t => {
   t.end();
 });
 
+test("subscribes to multiple events", async t => {
+  t.plan(7);
+
+  const p = new PhantomCore();
+
+  const origAListeners = p.listeners("mock-a");
+  const origBListeners = p.listeners("mock-b");
+  const origCListeners = p.listeners("mock-c");
+
+  const listenerSpy = sinon.spy((data: any) => null);
+
+  const unsubscribe = p.subscribe(listenerSpy, ["mock-a", "mock-b", "mock-c"]);
+
+  t.equals(p.listeners("mock-a").length, origAListeners.length + 1);
+  t.equals(p.listeners("mock-b").length, origBListeners.length + 1);
+  t.equals(p.listeners("mock-c").length, origCListeners.length + 1);
+
+  p.emit("mock-a", "test-data-a");
+
+  t.ok(listenerSpy.calledOnceWith("test-data-a"));
+
+  unsubscribe();
+
+  t.equals(p.listeners("mock-a").length, origAListeners.length);
+  t.equals(p.listeners("mock-b").length, origBListeners.length);
+  t.equals(p.listeners("mock-c").length, origCListeners.length);
+
+  await p.destroy();
+
+  t.end();
+});
+
 test("auto-unsubscribe on destruct", async t => {
   t.plan(1);
 
@@ -32,11 +64,8 @@ test("auto-unsubscribe on destruct", async t => {
   });
 
   const p = new PhantomCore();
-
   const unsubscribe = p.subscribe(() => null);
-
   const cleanupSpy = sinon.spy(p, "unregisterCleanupHandler");
-
   await p.destroy();
 
   t.ok(
